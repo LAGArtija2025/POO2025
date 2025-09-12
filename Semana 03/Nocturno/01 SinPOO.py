3a796c8336792a14defb1a5c6e8312bdbf4e20d1
LAGArtija2025 <lagaxyz2025@gmail.com>
2025-09-12 10:39:21 -0500

    Add files via upload

---
+++ Semana 03/Nocturno/01 SinPOO.py
@@ -0,0 +1,21 @@
+# Creando una fábrica de celulares sin POO
+
+celular1_marca = "Samsung"
+celular1_modelo = "S25"
+celular1_ram = "32GB"
+celular1_camaraF = "50MP"
+celular1_camaraT = "250MP"
+
+celular2_marca = "Oppo"
+celular2_modelo = "O16"
+celular2_ram = "24GB"
+celular2_camaraF = "12MP"
+celular2_camaraT = "24MP"
+
+celular3_marca = "Huawei"
+celular3_modelo = "H1"
+celular3_ram = "8GB"
+celular3_camaraF = "8MP"
+celular3_camaraT = "16MP"
+
+print(f"El celular 1 es de marca {celular1_marca} y modelo {celular1_modelo}")
